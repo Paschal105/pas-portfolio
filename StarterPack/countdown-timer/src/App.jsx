9cce2c1015f3edcd69e26576022a69a315058e30
Paschal <Ojatabupascal@gmail.com>
2025-07-09 01:07:28 -0400

    first commit

---
+++ StarterPack/countdown-timer/src/App.jsx
@@ -0,0 +1,15 @@
+
+ /* App.jsx */
+
+ import React from "react";
+ import CountdownTimer from "./CountDown";
+
+ function App() {
+  return (
+    <div className="App">
+      <CountdownTimer />
+    </div>
+  );
+}
+
+export default App;
